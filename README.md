9e113f42ecc4d24be339c9f78f7f3f0acb64ce66
GitElean <root@MSI>
2023-09-01 13:14:57 -0600

    readme

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# paralela_proyecto
+# paralela proyecto Screen Saver
+
+## Authors: Elean Rivas, Diego Ruiz
+
+El proyecto plantea ser un screen saver generado de forma paralela
+
+### Requisitos para compilar:
+- compilador GNDU de C++(g++, ninja, etc)
+- SDL2
+- OpenMP
+- Cmake v3.22
+- time
+
+
+### How to run
+Para ejecutar el programa solo es necesario ejecutar el script run.sh de la siguiente manera ./run.sh
+
+### Funcionalidad de los scripts
+configure: configuración de Cmake
+build: buildea el programa
+run: compila y corre el programa usando los scripts configure, build
+clean: limpia el build en caso de ser necesario
+
+### Trouble Shooting
+En caso de no tener las librerias ejecute los siguientes comandos(Debian)
+sudo apt-get install libsdl2-dev libomp-dev
+sudo apt install cmake
+
+En caso de no tener permisos para ejecutar los .sh ejecutar los siguientes comandos
+chmod +x configure.sh
+chmod +x build.sh
+chmod +x run.sh
+chmod +x clean.sh
+
+### Referencias
+https://github.com/denn1s/CC3096
+https://documentation.help/SDL/index.html
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
-#include <iostream>
 #include <SDL2/SDL.h>
-using namespace std;
-
+#include <stdlib.h>
+#include <time.h>
 
 typedef struct {
     int r;
@@ -9,14 +8,25 @@ typedef struct {
     int b;
 } Color;
 
-int window_width = 640;
-int window_height = 480;
+typedef struct {
+    int x;
+    int y;
+    int radius;
+    int velocity_x;
+    int velocity_y;
+    Color color;
+} Circle;
 
-int main() {
-  cout << "Hello World!";
-      SDL_Init(SDL_INIT_VIDEO);
+    int window_width = 640;
+    int window_height = 480;
 
-    SDL_Window *window = SDL_CreateWindow("Ventana de ejemplo",
+int main(int argc, char *argv[]) {
+
+    SDL_Init(SDL_INIT_VIDEO);
+
+
+
+    SDL_Window *window = SDL_CreateWindow("Screen Saver",
                                           SDL_WINDOWPOS_UNDEFINED,
                                           SDL_WINDOWPOS_UNDEFINED,
                                           window_width, window_height,
@@ -24,27 +34,28 @@ int main() {
 
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
 
-    Color color;
-    color.r = 200;
-    color.g = 200;
-    color.b = 0;
+    Color background_color;
+    background_color.r = 255;
+    background_color.g = 255;
+    background_color.b = 255;
 
-    SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, 255); // Color de fondo
+    int n = 5; // Número de círculos
+    Circle circles[n];
 
-    SDL_RenderClear(renderer); // Llena la ventana con el color de fondo
+    // Inicializar la semilla de números aleatorios
+    srand(time(NULL));
 
-    SDL_RenderPresent(renderer); // Muestra el color de fondo en la ventana
-
-    Color circle_color;
-    circle_color.r = 255;
-    circle_color.g = 0;
-    circle_color.b = 0;
-
-    int circle_radius = 50;
-    int circle_x = window_width / 2;
-    int circle_y = window_height / 2;
-    int circle_velocity_x = 20;
-    int circle_velocity_y = 20;
+    // Inicializar los círculos con posiciones, velocidades y colores aleatorios
+    for (int i = 0; i < n; i++) {
+        circles[i].x = rand() % window_width;
+        circles[i].y = rand() % window_height;
+        circles[i].radius = rand() % 50;
+        circles[i].velocity_x = rand() % 5 - 2;
+        circles[i].velocity_y = rand() % 5 - 2;
+        circles[i].color.r = rand() % 256;
+        circles[i].color.g = rand() % 256;
+        circles[i].color.b = rand() % 256;
+    }
 
     SDL_Event event;
     int running = 1;
@@ -56,40 +67,45 @@ int main() {
             }
         }
 
-        // Move the circle
-        circle_x += circle_velocity_x;
-        circle_y += circle_velocity_y;
+        // Mover y dibujar cada círculo
+        for (int i = 0; i < n; i++) {
+            // Mover el círculo
+            circles[i].x += circles[i].velocity_x;
+            circles[i].y += circles[i].velocity_y;
 
-        // Check for collision with the edges of the window
-        if (circle_x - circle_radius < 0 || circle_x + circle_radius > window_width) {
-            circle_velocity_x = -circle_velocity_x;
+            // Comprobar si el círculo choca con los bordes de la ventana
+            if (circles[i].x - circles[i].radius < 0 || circles[i].x + circles[i].radius > window_width) {
+                circles[i].velocity_x = -circles[i].velocity_x;
+            }
+
+            if (circles[i].y - circles[i].radius < 0 || circles[i].y + circles[i].radius > window_height) {
+                circles[i].velocity_y = -circles[i].velocity_y;
+            }
         }
 
-        if (circle_y - circle_radius < 0 || circle_y + circle_radius > window_height) {
-            circle_velocity_y = -circle_velocity_y;
-        }
+        // Limpiar la pantalla
+        SDL_SetRenderDrawColor(renderer, background_color.r, background_color.g, background_color.b, 255);
+        SDL_RenderClear(renderer);
 
-        // Clear the screen
-        //SDL_SetRenderDrawColor(renderer, background_color.r, background_color.g, background_color.b, 255);
-        //SDL_RenderClear(renderer);
+        // Dibujar cada círculo
+        for (int i = 0; i < n; i++) {
+            SDL_SetRenderDrawColor(renderer, circles[i].color.r, circles[i].color.g, circles[i].color.b, 255);
 
-        // Draw the circle
-        SDL_SetRenderDrawColor(renderer, circle_color.r, circle_color.g, circle_color.b, 255);
-
-        for (int w = 0; w < circle_radius * 2; w++) {
-            for (int h = 0; h < circle_radius * 2; h++) {
-                int dx = circle_radius - w;
-                int dy = circle_radius - h;
-                if ((dx*dx + dy*dy) <= (circle_radius * circle_radius)) {
-                    SDL_RenderDrawPoint(renderer, circle_x + dx - circle_radius, circle_y + dy - circle_radius);
+            for (int w = 0; w < circles[i].radius * 2; w++) {
+                for (int h = 0; h < circles[i].radius * 2; h++) {
+                    int dx = circles[i].radius - w;
+                    int dy = circles[i].radius - h;
+                    if ((dx*dx + dy*dy) <= (circles[i].radius * circles[i].radius)) {
+                        SDL_RenderDrawPoint(renderer, circles[i].x + dx - circles[i].radius, circles[i].y + dy - circles[i].radius);
+                    }
                 }
             }
         }
 
-        // Present the renderer
+        // Presentar el renderer
         SDL_RenderPresent(renderer);
 
-        // Wait for a little bit
+        // Esperar un poco
         SDL_Delay(10);
     }
 
